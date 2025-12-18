9db084fab116aa5182d85b0167a9ac960a3e1352
Jonathon Ringeisen <ringeisen85@gmail.com>
2025-12-18 08:37:38 -0500

    - Creates a vite manifest for testing purposes in CI.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Vite;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
@@ -11,5 +12,33 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         config()->set('scout.driver', 'collection');
+        $this->ensureViteManifestExists();
+    }
+
+    protected function ensureViteManifestExists(): void
+    {
+        /** @var Vite $vite */
+        $vite = app(Vite::class);
+        $manifestPath = public_path($vite->isRunningHot() ? 'hot/manifest.json' : 'build/manifest.json');
+
+        if (! is_file($manifestPath)) {
+            if (! is_dir(dirname($manifestPath))) {
+                mkdir(dirname($manifestPath), recursive: true);
+            }
+
+            file_put_contents(
+                $manifestPath,
+                json_encode([
+                    'resources/js/app.js' => [
+                        'file' => 'assets/app.js',
+                        'isEntry' => true,
+                    ],
+                    'resources/css/app.css' => [
+                        'file' => 'assets/app.css',
+                        'isEntry' => true,
+                    ],
+                ], JSON_PRETTY_PRINT)
+            );
+        }
     }
 }
